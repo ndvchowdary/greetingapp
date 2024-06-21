eae6aa5e44f1e037c1bae0bd8dfc624352fed049
ndvchowdary <157372726+ndvchowdary@users.noreply.github.com>
2024-06-21 17:23:51 +0800

    Update greetingapp.java

--- greetingapp.java
+++ greetingapp.java
@@ -4,7 +4,7 @@ public class GreetingApp {
 
     public static void main(String[] args) {
         String myName = "Nani";
-        System.out.println("Hello, " + myName + "! Welcome to Maven.");
+        System.out.println("Hey nani how r u, " + myName + "! Welcome to Maven.");
     }
 }
 

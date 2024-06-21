145448a932cf01a8abdd860e7729afedeeaa87bf
root <root@ip-172-31-62-212.ec2.internal>
2024-06-21 07:53:56 +0000

    sending all folders

---
+++ greetingapp.java
@@ -0,0 +1,10 @@
+package com.nani;
+
+public class GreetingApp {
+
+    public static void main(String[] args) {
+        String myName = "Nani";
+        System.out.println("Hello, " + myName + "! Welcome to Maven.");
+    }
+}
+
